eb600fea4a7f7391523620803b769b0e5e6eaa3f
kenny1120 <kenny.bello@hotmail.com>
2013-05-07 19:52:25 -0500

    javadoc (1)

--- HubbsCenter/src/com/example/dissentfinal/AboutScreen.java
+++ HubbsCenter/src/com/example/dissentfinal/AboutScreen.java
@@ -7,9 +7,22 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+/**
+ * AboutScreen class displays the About Screen layout and is an Activity
+ * It implements the OnClickListener that allows the user to press a button that sends them back to the Menu Activity
+ * @author kennethbello
+ *
+ */
 public class AboutScreen extends Activity implements OnClickListener{
+	/**
+	 * about_menu button allows the user to return to the Menu Activity when clicked
+	 */
 	Button about_menu;
 	
+	/**
+	 * onCreate initializes the Activity (AboutScreen)
+	 * it is called when the Activity starts
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -19,7 +32,10 @@ public class AboutScreen extends Activity implements OnClickListener{
 		about_menu.setBackgroundColor(Color.TRANSPARENT);
 		
 	}
-
+	
+	/**
+	 * onCLick method called when a view has been clicked
+	 */
 	@Override
 	public void onClick(View v) {
 		finish();
